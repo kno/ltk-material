421ef7ff9b5cde2b384b7b6483f1952535d0b32e
linsms <linsms@gmail.com>
2018-11-29 13:25:09 +0000

    Initial Commit

---
+++ src/main/groovy/material/domain/lntkEntity.groovy
@@ -0,0 +1,21 @@
+package material.domain
+
+import grails.gorm.annotation.Entity
+
+@Entity
+class LntkEntity{
+    String id
+    String reference
+    String name
+    Date creationDate
+    Date updateDate
+    String tenantId
+    String plantId
+    String createdById
+    String updateById
+    String description
+    static mapping = {
+        id generator: 'uuid'
+    }
+
+}
---
+++ src/main/groovy/material/domain/material.groovy
@@ -0,0 +1,8 @@
+package material.domain
+
+import grails.gorm.annotation.Entity
+
+@Entity
+class Material extends LntkEntity{
+    Double price  
+}
